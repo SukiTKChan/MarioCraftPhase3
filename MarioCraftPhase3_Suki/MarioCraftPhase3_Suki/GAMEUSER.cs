56984294282447c65944d499729c98b75db7ec0a
SukiTKChan <sukitkchan@gmail.com>
2016-11-23 22:28:02 +0000

    Initial Commit
    Set up project on Visual Studio
    Imported Entity Framework

---
+++ MarioCraftPhase3_Suki/MarioCraftPhase3_Suki/GAMEUSER.cs
@@ -0,0 +1,30 @@
+namespace MarioCraftPhase3_Suki
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ADMIN.GAMEUSER")]
+    public partial class GAMEUSER
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public GAMEUSER()
+        {
+            PLAYERs = new HashSet<PLAYER>();
+        }
+
+        [Key]
+        public byte USERID { get; set; }
+
+        [StringLength(20)]
+        public string USEREMAIL { get; set; }
+
+        [StringLength(20)]
+        public string USERPASSWORD { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PLAYER> PLAYERs { get; set; }
+    }
+}
